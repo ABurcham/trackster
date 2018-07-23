00e4d59549bb5aef93ac5ba19dfb285457985786
Apollos Burcham <Apollos@MacBook-Pro.local>
2018-07-23 12:21:37 -0500

    Complete the Query for the Last.fm API

--- resources/js/trackster.js
+++ resources/js/trackster.js
@@ -4,6 +4,8 @@ $(document).ready(function(){
     Trackster.searchTracksByTitle($('#search-input').val());
   });
 });
+
+const API_KEY = '439b2b133f043b578cf1b99d6ca39ef3';
 /*
   Given an array of track data, create the HTML for a Bootstrap row for each.
   Append each "row" to the container in the body to display all tracks.
@@ -17,6 +19,11 @@ Trackster.renderTracks = function(tracks) {
   Render the tracks given in the API query response.
 */
 Trackster.searchTracksByTitle = function(title) {
-
+  $.ajax({
+    url: 'https://ws.audioscrobbler.com/2.0/?method=track.search&track=' + title + '&api_key=' + API_KEY + '&format=json',
+    success: function(response) {
+      Trackster.renderTracks(response.results.trackmatches.track);
+    }
+  });
 };
 /*User Interactions with the Button*/
